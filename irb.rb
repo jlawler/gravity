7a8f33df55affafa485f4117c24b576f2e4f816d
Jeremy Lawler (morgoth) <jeremylawler@gmail.com>
2018-01-08 09:27:32 -0600

    Starting to decouple render from calculation

--- irb.rb
+++ irb.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'gosu'
-require 'chipmunk'
 
 $:.unshift './lib'
 
 require 'config_parser'
 require 'gosu_circle'
-require 'constants'
 require 'frame'
 
+def velocity_for_orbit(range_in_au)
+  sun_mass = 1.989*(10**30)
+  (Frame::G_IN_AU_PER_KG_D2 * sun_mass)**0.5
+
+end
 
